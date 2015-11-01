97ac0e5042be3ff8f02fc9bad3fdcb6efb12c770
Walter Schulze <awalterschulze@gmail.com>
2015-11-01 14:26:14 +0200

    install deps on travis

---
+++ install-dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xe
+
+mkdir -p $GOPATH/src/github.com/gogo
+git clone https://github.com/gogo/protobuf $GOPATH/src/github.com/gogo/protobuf
+mkdir -p $GOPATH/src/github.com/katydid
+git clone https://github.com/katydid/katydid $GOPATH/src/github.com/katydid/katydid
+(cd $GOPATH/src/github.com/katydid/katydid && git checkout prototype)
